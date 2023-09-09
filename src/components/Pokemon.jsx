b29bcd76e274f87911b471b9e497c87f3cdd0e0f
Lucasvillalba2525 <villalbalucas.dani@gmail.com>
2023-09-09 17:29:37 -0300

    commit

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -15,22 +15,17 @@ export default function Pokemon({ pokemon }) {
     return (
             <div className="row">
                 <div className="col p-4 d-flex flex-column position-static">
+                    
                     <h3 className="mb-0">{pokemonId.name}</h3>
-                    {/* <h3 className="mb-0">{pokemonId.forms.name}</h3> */}
                     <div className="mb-1 text-body-secondary">Nov 12</div>
                     <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
 
-                    {/* <Link to={`/Detalle/${pokemonId.id}`} className="btn btn-info rounded-pill px-3">Mas Info</Link> */}
                     <Link to={`/Detalle/${pokemonId.id}`} className="button">Mas Info</Link>
                 </div>
                 <div>
                     <img src={pokemonImg.front_default}
                          width="200" height="250"
                     ></img>
-                    {/* imagen mala 
-                    <img src={pokemonId.sprites.front_default}
-                         width="200" height="250"
-                    ></img> */}
                 </div>
             </div>
     )
