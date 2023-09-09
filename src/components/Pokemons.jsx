3ae55561660a169e56292608d6ae895072069151
Lucasvillalba2525 <villalbalucas.dani@gmail.com>
2023-09-09 18:34:13 -0300

    a veer

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -4,25 +4,20 @@ import Pokemon from './pokemon';
 
 export default function Pokemons() {
     const [pokemons,setPokemons] = useState([]);
-    const [pokemonId, setPokemonId] = useState([]);
-
-    useEffect(() => {
-        obtenerPokemonByUrl(pokemons.url).then((data) => {
-            setPokemonId(data);
-        })
-    }, [])
+    var id = 0;
 
     useEffect(() => {
         obtenerPokemons().then((data) => {
             // console.log(data);
             setPokemons(data);
+            
         })
     },[])
     
   return (
     <div className='main'>
         {
-            pokemons.map(pokemon => <Pokemon key={pokemonId.id} pokemon={pokemon}/>)
+            pokemons.map(pokemon => <Pokemon key={id++} pokemon={pokemon}/>)
         }
     </div>  
   )
